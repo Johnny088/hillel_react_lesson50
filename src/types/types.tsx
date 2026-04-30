48f1fd65c07b13aff57bdec9024822657d622ab0
Johnny <espgoodlife22@gmail.com>
2026-04-30 19:22:44 +0300

    test

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,3 +1,17 @@
+type hobbyList =
+  | 'skydiving'
+  | 'singing'
+  | 'bearDrinking'
+  | 'fishing'
+  | 'videogaming'
+  | 'working out'
+  | 'traveling'
+  | 'reading'
+  | 'studying'
+  | ' learning lenguages'
+  | 'other'
+  | 'taking photos'
+  | 'dansing';
 export interface User {
   name: string;
   surname: string;
@@ -5,4 +19,12 @@ export interface User {
   confirmPassword: string;
   email: string;
   phone: string;
+  country: string;
+  city: string;
+  postalCode: number;
+  birthDate: Date;
+  sex: 'male' | 'female';
+  hobbies: hobbyList[];
+  personality: string;
+  isConfirmRules: boolean;
 }
