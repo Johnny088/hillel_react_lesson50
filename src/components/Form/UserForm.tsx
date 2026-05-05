8f9052108bdb83ecf0e1f94c40eaae3161e9ca37
Johnny <espgoodlife22@gmail.com>
2026-05-05 20:10:26 +0300

    done

--- src/components/Form/UserForm.tsx
+++ src/components/Form/UserForm.tsx
@@ -2,6 +2,7 @@ import { Field, Formik, ErrorMessage, Form, type FormikHelpers } from 'formik';
 import type { User } from '../../types/types';
 import * as yup from 'yup';
 import css from './Form.module.css';
+import { isValidElement } from 'react';
 
 const initialValues: User = {
   name: '',
@@ -71,7 +72,7 @@ const userSchema = yup.object().shape({
 export const UsersForm = () => {
   const handleSubmit = (values: User, formikHelpers: FormikHelpers<User>) => {
     console.log(values);
-    // formikHelpers.resetForm();
+    formikHelpers.resetForm();
   };
   return (
     <Formik
@@ -79,170 +80,198 @@ export const UsersForm = () => {
       onSubmit={handleSubmit}
       validationSchema={userSchema}
     >
-      <Form className={css.formContainer}>
-        <label>
-          Name: <Field type="text" name="name" />
-          <ErrorMessage name="name" className={css.errorState} component="p" />
-        </label>
-
-        <label>
-          Surname: <Field type="text" name="surname" />
-          <ErrorMessage
-            name="surname"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
-
-        <label>
-          Email: <Field type="email" name="email" />
-          <ErrorMessage name="email" className={css.errorState} component="p" />
-        </label>
-
-        <label>
-          Phone: <Field type="text" name="phone" />
-          <ErrorMessage name="phone" className={css.errorState} component="p" />
-        </label>
-
-        <label>
-          Country: <Field type="text" name="country" />
-          <ErrorMessage
-            name="country"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
-
-        <label>
-          City: <Field type="text" name="city" />
-          <ErrorMessage name="city" className={css.errorState} component="p" />
-        </label>
-
-        <label>
-          Postal code: <Field type="text" name="postalCode" />
-          <ErrorMessage
-            name="postalCode"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
-
-        <label>
-          Birthdate: <Field type="date" name="birthDate" />
-          <ErrorMessage
-            name="birthDate"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
-
-        <label>
-          <Field type="radio" value="male" name="sex" />
-          male
-        </label>
-        <label>
-          <Field value="female" type="radio" name="sex" />
-          female
-        </label>
-        <label>
-          <Field value="other" type="radio" name="sex" />
-          other
-        </label>
-        <fieldset className={css.hobyContainer}>
-          <legend>Hobbies</legend>
+      {({ isValid, dirty }) => (
+        <Form className={css.formContainer}>
           <label>
-            <Field type="checkbox" value="skydiving" name="hobbies" />
-            skydiving
+            Name: <Field type="text" name="name" />
+            <ErrorMessage
+              name="name"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            Surname: <Field type="text" name="surname" />
+            <ErrorMessage
+              name="surname"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            Email: <Field type="email" name="email" />
+            <ErrorMessage
+              name="email"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            Phone: <Field type="text" name="phone" />
+            <ErrorMessage
+              name="phone"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            Country: <Field type="text" name="country" />
+            <ErrorMessage
+              name="country"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            City: <Field type="text" name="city" />
+            <ErrorMessage
+              name="city"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            Postal code: <Field type="text" name="postalCode" />
+            <ErrorMessage
+              name="postalCode"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            Birthdate: <Field type="date" name="birthDate" />
+            <ErrorMessage
+              name="birthDate"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+
+          <label>
+            <Field type="radio" value="male" name="sex" />
+            male
           </label>
           <label>
-            <Field type="checkbox" value="singing" name="hobbies" />
-            singing
+            <Field value="female" type="radio" name="sex" />
+            female
           </label>
           <label>
-            <Field type="checkbox" value="fishing" name="hobbies" />
-            fishing
+            <Field value="other" type="radio" name="sex" />
+            other
           </label>
-          <label>
-            <Field type="checkbox" value="videogaming" name="hobbies" />
-            videogaming
-          </label>
-          <label>
-            <Field type="checkbox" value="working out" name="hobbies" />
-            working out
-          </label>
-          <label>
-            <Field type="checkbox" value="traveling" name="hobbies" />
-            traveling
-          </label>
-          <label>
-            <Field type="checkbox" value="reading" name="hobbies" />
-            reading
-          </label>
-          <label>
-            <Field type="checkbox" value="studying" name="hobbies" />
-            studying
-          </label>
-          <label>
-            <Field type="checkbox" value="learning lenguages" name="hobbies" />
-            learning lenguages
-          </label>
-          <label>
-            <Field type="checkbox" value="taking photos" name="hobbies" />
-            taking photos
-          </label>
-          <label>
-            <Field type="checkbox" value="dansing" name="hobbies" />
-            dansing
-          </label>
-          <label>
-            <Field type="checkbox" value="other" name="hobbies" />
-            'other'
-          </label>
-          <ErrorMessage
-            name="hobbies"
-            className={css.errorState}
-            component="p"
-          />
-        </fieldset>
+          <fieldset className={css.hobyContainer}>
+            <legend>Hobbies</legend>
+            <label>
+              <Field type="checkbox" value="skydiving" name="hobbies" />
+              skydiving
+            </label>
+            <label>
+              <Field type="checkbox" value="singing" name="hobbies" />
+              singing
+            </label>
+            <label>
+              <Field type="checkbox" value="fishing" name="hobbies" />
+              fishing
+            </label>
+            <label>
+              <Field type="checkbox" value="videogaming" name="hobbies" />
+              videogaming
+            </label>
+            <label>
+              <Field type="checkbox" value="working out" name="hobbies" />
+              working out
+            </label>
+            <label>
+              <Field type="checkbox" value="traveling" name="hobbies" />
+              traveling
+            </label>
+            <label>
+              <Field type="checkbox" value="reading" name="hobbies" />
+              reading
+            </label>
+            <label>
+              <Field type="checkbox" value="studying" name="hobbies" />
+              studying
+            </label>
+            <label>
+              <Field
+                type="checkbox"
+                value="learning lenguages"
+                name="hobbies"
+              />
+              learning lenguages
+            </label>
+            <label>
+              <Field type="checkbox" value="taking photos" name="hobbies" />
+              taking photos
+            </label>
+            <label>
+              <Field type="checkbox" value="dansing" name="hobbies" />
+              dansing
+            </label>
+            <label>
+              <Field type="checkbox" value="other" name="hobbies" />
+              'other'
+            </label>
+            <ErrorMessage
+              name="hobbies"
+              className={css.errorState}
+              component="p"
+            />
+          </fieldset>
 
-        <label>
-          Password: <Field type="password" name="password" />
-          <ErrorMessage
-            name="password"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
+          <label>
+            Password: <Field type="password" name="password" />
+            <ErrorMessage
+              name="password"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
 
-        <label>
-          Confirm Password: <Field type="password" name="confirmPassword" />
-          <ErrorMessage
-            name="confirmPassword"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
+          <label>
+            Confirm Password: <Field type="password" name="confirmPassword" />
+            <ErrorMessage
+              name="confirmPassword"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
 
-        <label>
-          Personality: <Field as="textarea" name="personality" />
-          <ErrorMessage
-            name="personality"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
+          <label>
+            Personality: <Field as="textarea" name="personality" />
+            <ErrorMessage
+              name="personality"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
 
-        <label>
-          Confirm rules:
-          <Field type="checkbox" name="isConfirmRules" />
-          <ErrorMessage
-            name="isConfirmRules"
-            className={css.errorState}
-            component="p"
-          />
-        </label>
-        <button className={css.submitBtn}> register</button>
-      </Form>
+          <label>
+            Confirm rules:
+            <Field type="checkbox" name="isConfirmRules" />
+            <ErrorMessage
+              name="isConfirmRules"
+              className={css.errorState}
+              component="p"
+            />
+          </label>
+          <button
+            type="submit"
+            disabled={!(isValid && dirty)}
+            className={!(isValid && dirty) ? css.notValidBtn : css.submitBtn}
+          >
+            send
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
